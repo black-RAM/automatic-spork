eb52961287251ac2272e717e4e407edd235c342e
black-RAM <pplanes311@gmail.com>
2024-02-27 08:39:47 +0300

    feat: tests for reverseString() and calculator object

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ function capitalize(word){
   return word[0].toUpperCase() + word.slice(1).toLowerCase()
 }
 
-export {capitalize}
+function reverseString(string) {
+  return string.split('').reverse().join('')
+}
+
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b
+}
+
+export {capitalize, reverseString, calculator}
--- src/test.js
+++ src/test.js
@@ -1,5 +1,23 @@
-import { capitalize } from "."
+import { capitalize, reverseString, calculator } from "."
 
 test("capitalize('tERmInal') returns 'Terminal'.", () => {
   expect(capitalize("tERmInal")).toBe("Terminal")
 })
+
+test("revereString('palindromes') returns 'semordnilap'.", () => {
+  expect(reverseString("palindromes")).toBe("semordnilap")
+})
+
+const [a, b] = [4, 2]
+const driver = [
+  {name: "add", return: a + b}, 
+  {name: "subtract", return: a - b}, 
+  {name: "multiply", return: a * b}, 
+  {name: "divide", return: a / b},
+]
+
+for (const func of driver) {
+  test(`calculator.${func.name}(${a}, ${b}) returns ${func.return}`, () => {
+    expect(calculator[func.name](a, b)).toBe(func.return)
+  })
+}
